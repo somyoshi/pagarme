09e9c4284139ea5c001195ab6813eaf9be2728c0
Bruno Costa <somyoshi@gmail.com>
2020-03-30 09:09:55 -0300

    xd

--- src/pagarme.ts
+++ src/pagarme.ts
@@ -2,7 +2,7 @@ const pagarme = require('pagarme');
 
 export async function connect(): Promise<any> {
     return new Promise<any>((resolve, reject) => {
-        pagarme.client.connect({ api_key: 'ak_test_RfUiFy5sg1kctJf98a6h0u68UArJ7b' })
+        pagarme.client.connect({ api_key: '#####' })
             .then((client:any) => resolve(client))
             //.then((transactions:any) => console.log(transactions))
             .catch((error:any) => console.error(error))
